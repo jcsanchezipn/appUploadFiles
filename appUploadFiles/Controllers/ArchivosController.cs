3f183ef902863dcda84b64e3a1645dda373349d7
Sanchez <jcsanchez@ipntijuana.mx>
2020-12-11 17:14:01 -0800

    Ejemplo clase

---
+++ appUploadFiles/Controllers/ArchivosController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using appUploadFiles.Models;
+
+namespace appUploadFiles.Controllers
+{
+    public class ArchivosController : Controller
+    {
+        // GET: Arcvhios
+        public ActionResult Index()
+        {
+            if(TempData["msg"] != null)
+            {
+                ViewBag.msg = TempData["msg"];
+            }
+            string wwwPath = Server.MapPath("~/");
+            string[] filesEntries = Directory.GetFiles(wwwPath + "/files");
+            ViewBag.filesEntries = filesEntries;
+
+
+            var onlyFileNames = new List<string>();
+            foreach (string fn in ViewBag.filesEntries)
+            {
+                onlyFileNames.Add(Path.GetFileName(fn));
+            }
+
+            var myArrayWithFN = onlyFileNames.ToArray();
+
+            ViewBag.myArrayFN = myArrayWithFN;
+
+            return View();
+        }
+
+
+        public ActionResult SaveFiles(FileToUploadViewModel model)
+        {
+            
+            if (!ModelState.IsValid)
+            {
+                return View("Index", model);
+            }
+            
+            
+            string wwwPath = Server.MapPath("~/");
+            string pathFiles = Path.Combine(wwwPath + "/files/");
+
+            model.fileUpload01.SaveAs(pathFiles + model.fileUpload01.FileName);
+            model.fileUpload02.SaveAs(pathFiles + model.fileUpload02.FileName);
+            TempData["msg"] = "Archivos almacenados en la nube";
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ appUploadFiles/Models/FileToUploadViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace appUploadFiles.Models
+{
+    public class FileToUploadViewModel
+    {
+        [Required]
+        [DisplayName("Mi Archivo que arriba va")]
+        public HttpPostedFileBase fileUpload01 { get; set; }
+        [Required]
+        public HttpPostedFileBase fileUpload02 { get; set; }
+        public string texto { get; set; }
+    }
+}
